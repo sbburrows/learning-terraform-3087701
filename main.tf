6d29475de8b3f3b9079b721136c546559a43ee38
Steve Burrow <sbburrows@gmail.com>
2025-03-05 16:59:16 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.nano"
 
   tags = {
